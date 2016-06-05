5b207c1a50ed469cf19bb3e49cf9e1f2699e5d56
HanManHyuk <1manhyuk@gmail.com>
2016-06-05 21:26:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,9 +24,10 @@ var App = {
       "#\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t                                                             #\n"+
       "#\t\tHi, I'm " + commandText('ManHYuk') + " - Backend Web Developer in Korea.\t\t\t\t\t\t\t\t\t\t\t\t                        #\n"+
       "#\t\tPlease type " +commandText('ls')+" or " +commandText('help')+ " for a list of commands.  \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t #\n"+
+      "#\t\t(Sorry, This is not complete Page. Please, Show Outline) \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t#\n"+
       "#\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t     #\n"+
       "###################################################################################################################################\n"
-      "Sorry, This is not complete page. Please, Show outline";
+      \;
     if (!ret) {
         this.echo("\n" + greetText);
     } else {
